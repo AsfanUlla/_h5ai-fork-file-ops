635128cefaf8c7ea95f490ab8c3400ae882f9e3f
AsfanUlla <asfanulla@gmail.com>
2020-10-21 14:47:03 +0530

    Update db.php

--- _h5ai/public/php/db.php
+++ _h5ai/public/php/db.php
@@ -1,5 +1,5 @@
 <?php
-$con = new mysqli("localhost", "root", "n0t34sy%", "user");
+$con = new mysqli("localhost", "_USER_", "_PASSWORD_", "_DB_");
 
 if ($con->connect_error) {
     die("Connection failed: " . $con->connect_error);
